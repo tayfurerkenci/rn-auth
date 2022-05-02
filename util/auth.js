6073610548349acc4eaa6511e688f9a299b66399
Tayfur Erkenci <terkenci@gmail.com>
2022-05-02 14:41:25 +0300

    api key hid

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY='AIzaSyBwMsWpTUznGtq2qwXwnnURS33hmodFCpE';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const createUser = async (email, password) => {
     await authenticate('signUp', email, password);
